bd15f26874b39c9681fb2e5ca7f469f8708f95b8
Angel <joseam7112@gmail.com>
2020-09-10 22:47:06 -0500

    primer respaldo

---
+++ docs/precache-manifest.78e0f2c390ddf954adc2b1ddd08f6827.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56e6b8f323d714b7a1aabc8914fab509",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cbfe1db65b3bc8b7322f",
+    "url": "/static/css/main.5e99e198.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbfe1db65b3bc8b7322f",
+    "url": "/static/js/main.b82ba932.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
